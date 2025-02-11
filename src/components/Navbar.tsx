2fb0355cb948b4493f39f9db809278fd472404ba
Gauravc07 <gauravchindhe1806@gmail.com>
2025-02-12 00:29:55 +0530

    fourth commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
 
 // Import the logo image
-import logo from '../assests/logo.png'; 
+import logo from '../assests/pictures/meraki_logo.png'; 
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
